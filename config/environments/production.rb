bafe9e121f3b859ae37797f7b48b10cd414837b4
Andrew Pasquale <andrew@elytra.net>
2014-09-14 16:29:48 +0000

    Add default_url for action_mailer.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,8 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_mailer.default_url_options = { :host => 'culturemap.us' }
+  Rails.application.routes.default_url_options = config.action_mailer.default_url_options
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
